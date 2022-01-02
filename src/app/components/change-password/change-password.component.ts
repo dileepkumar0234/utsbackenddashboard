b8d54a2c1d99bceb77ab22dc6a18b4f83c263984
Kranthi7G <kranthi7g@gmail.com>
2022-01-02 22:38:17 +0530

    Change Password.

---
+++ src/app/components/change-password/change-password.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonService } from 'src/app/common.service';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss']
+})
+export class ChangePasswordComponent implements OnInit {
+
+  public passwordForm: FormGroup;
+  confirmpasswordflag = false;
+  submitted = false;
+  success = "";
+  failed = "";
+  
+  constructor(private formBuilder : FormBuilder, private commonService : CommonService, private apiService : ApiService) {
+
+    this.passwordForm = this.formBuilder.group({
+      oldpassword: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmpassword: ['', [Validators.required, Validators.minLength(4)]],
+    },
+    {
+      validator: this.ConfirmedValidator('password', 'confirmpassword')
+    })
+   }
+
+  get f() { return this.passwordForm.controls; }
+
+
+  changePassword()
+  {
+    this.failed = "";
+    this.success = "";
+    if (this.passwordForm.invalid) {
+      this.commonService.validateAllFormFields(this.passwordForm);
+      return;
+    }
+   
+    this.apiService.postCall('/user/changepassword', this.passwordForm.getRawValue())
+    .subscribe(
+      res => {
+        this.passwordForm.reset();
+        this.success = res.status_smessage; 
+        setTimeout(() => {
+          this.success = "";
+        }, 4000);
+      },
+      error => {
+        this.failed = "Please try again later."
+        setTimeout(() => {
+          this.failed = "";
+        }, 4000);
+      }
+    )
+  }
+
+  ConfirmedValidator(controlName: string, matchingControlName: string){
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ confirmedValidator: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+}
--- src/app/components/to-call/to-call.component.ts
+++ src/app/components/to-call/to-call.component.ts
@@ -1,4 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { ApiService } from 'src/app/services/api.service';
+import { CommonService } from 'src/app/services/common.service';
+
+
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
+}
+
+interface Person {
+  contact_id: any;
+  c_name: any;
+  c_email: any;
+  c_phone: any;
+  c_message: any;
+  c_created_at: any;
+}
+
 
 @Component({
   selector: 'app-to-call',
@@ -7,9 +29,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToCallComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+
+  userId : any;
+
+  taxYear : any;
+
+  persons: Person[] = [];
+  
+  constructor(private http: HttpClient , 
+    private authService : AuthService,
+    private commonService : CommonService,
+    private apiService : ApiService) {
+
+    this.userId = this.authService.getUserId();
+    
+    this.taxYear = this.authService.getTaxYear();
+   }
 
   ngOnInit(): void {
+    this.getTableData();
+  }
+  
+  getTableData()
+  {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      serverSide: true,
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        console.log('All Params', dataTablesParameters);
+        this.http
+          .post<DataTablesResponse>(
+            this.apiService.baseUrl +'/user/wantusinfo',
+            {dataTablesParameters, user_id : this.userId, taxYear : this.taxYear}, 
+            {}
+          )
+          .subscribe(resp => {
+            this.persons = resp.data;
+            console.log(resp);
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: []
+            });
+          });
+      },
+      columns: [{ data: 'c_name' }, { data: 'c_email' }, { data: 'c_phone' }, 
+                { data: 'c_message' }, { data: 'c_created_at' }]
+    };
   }
 
+  OnDelete(contactId : any)
+  {
+    if (confirm("Please confirm to delete"))
+    {
+      this.apiService.postCall('/user/deletewantuscall', {contactid : contactId})
+      .subscribe(
+        res => {
+          console.log(res);
+          this.commonService.refresh();
+        },
+        error => {
+        }
+      )
+    }
+
+  }
 }
