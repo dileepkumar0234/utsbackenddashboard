e7d3fa5418c73c1e0dccae8fac467519be51e68a
dileepkumar0234 <dileepkumarkonda@gmail.com>
2022-07-02 12:04:56 +0530

    Message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,15 @@
 export const environment = {
   production: true,
-  // apiUrl:'http://localhost:8626/api',
-  apiUrl:'http://13.127.242.35:8626/api',
+  apiUrl:'http://demo.umpiretaxsolutions.com:8626/api',
+  // apiUrl:'https://www.umpiretaxsolutions.com:8626/api',
+  main_site_url:"http://demo.umpiretaxsolutions.com",
+  // main_site_url:"https://www.umpiretaxsolutions.com",
+  base_url:"http://demo.umpiretaxsolutions.com/UTS-OfficialTaxFilers",
+  // base_url:"https://www.umpiretaxsolutions.com/UTS-OfficialTaxFilers",
   user_id:'user_id',
   uu_id:'uu_id',
   taxYear:'tax_year',
   role:'role',
   logo : './assets/images/logo.svg',
   profile : './assets/images/profile.png'
-};
+}; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,12 @@
 
 export const environment = {
   production: false,
-  // apiUrl:'http://localhost:8626/api',
-  apiUrl:'http://13.127.242.35:8626/api',
+  apiUrl:'http://demo.umpiretaxsolutions.com:8626/api',
+  // apiUrl:'https://www.umpiretaxsolutions.com:8626/api',
+  main_site_url:"http://demo.umpiretaxsolutions.com",
+  // main_site_url:"https://www.umpiretaxsolutions.com",
+  base_url:"http://demo.umpiretaxsolutions.com/UTS-OfficialTaxFilers",
+  // base_url:"https://www.umpiretaxsolutions.com/UTS-OfficialTaxFilers",
   user_id:'user_id',
   uu_id:'uu_id',
   taxYear:'tax_year',
