94355ace85c6ff893df62b84a5b1312d7da23b40
dileepkumar0234 <dileepkumarkonda@gmail.com>
2022-11-05 22:12:23 +0530

    Form-select

--- dist/utsuiwebsite/assets/js/custom.js
+++ dist/utsuiwebsite/assets/js/custom.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-    $('.select2').select2(); //initialize 
-});
+// $(document).ready(function() {
+//     $('.select2').select2(); //initialize
+// });
+
+
+function load_select(){
+  $('.select2').select2();
+}
+
+function get_value(id) {
+  return $("#"+id).val();
+}
+
+function onchange_value(event) {
+  if (event.value != "") {
+    $('#'+event.id+'_error').css('display','none');
+  } else $('#'+event.id+'_error').css('display','block');
+}
